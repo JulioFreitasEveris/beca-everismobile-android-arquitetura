79b9c3fc56cd62d7d996dab55d7bfcf4e5dd8cde
Julio Freitas <julio.cesar.freitas@everis.com>
2021-04-05 09:20:39 -0300

    Adcionando extensões

--- app/src/main/java/com/everis/becakotlinmvvm/api/ApiInterface.kt
+++ app/src/main/java/com/everis/becakotlinmvvm/api/ApiInterface.kt
@@ -3,9 +3,11 @@ package com.everis.becakotlinmvvm.api
 import com.everis.becakotlinmvvm.view.HolidayModel
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiInterface {
 
     @GET("PublicHolidays/2021/br")
     fun getHolidays(): Call<List<HolidayModel>>
+
 }
